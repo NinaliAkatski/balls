471cc370f94b8241725b7402f1c8cc3dbf87a81a
NinaliAkatski <51315178+NinaliAkatski@users.noreply.github.com>
2019-06-04 17:26:59 +0900

    Add files via upload

---
+++ js.js
@@ -0,0 +1,73 @@
+let balls = document.querySelectorAll(".ball")
+let score = 0;
+// balls
+balls[0].onclick = function(){
+    score = score + 1;
+    balls[0].style.display = "none";
+    document.querySelector(".score").innerHTML = score;
+    
+}
+
+balls[1].onclick = function(){
+    score = score + 1;
+    balls[1].style.display = "none";
+    document.querySelector(".score").innerHTML = score;
+    
+}
+
+balls[2].onclick = function(){
+    score = score + 1;
+    balls[2].style.display = "none";
+    document.querySelector(".score").innerHTML = score;
+    
+}
+
+balls[3].onclick = function(){
+    score = score + 1;
+    balls[3].style.display = "none";
+    document.querySelector(".score").innerHTML = score;
+    
+}
+
+balls[4].onclick = function(){
+    score = score + 1;
+    balls[4].style.display = "none";
+    document.querySelector(".score").innerHTML = score;
+    
+}
+
+balls[5].onclick = function(){
+    score = score + 1;
+    balls[5].style.display = "none";
+    document.querySelector(".score").innerHTML = score;
+    
+}
+
+balls[6].onclick = function(){
+    score = score + 1;
+    balls[6].style.display = "none";
+    document.querySelector(".score").innerHTML = score;
+    
+}
+
+balls[7].onclick = function(){
+    score = score + 1;
+    balls[7].style.display = "none";
+    document.querySelector(".score").innerHTML = score;
+    
+}
+
+balls[8].onclick = function(){
+    score = score + 1;
+    balls[8].style.display = "none";
+    document.querySelector(".score").innerHTML = score;
+    
+}
+
+balls[9].onclick = function(){
+    score = score + 1;
+    balls[9].style.display = "none";
+    document.querySelector(".score").innerHTML = score;
+    
+}
+
